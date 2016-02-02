0b9baf9f1304d8064384d22e352c851ef9a06ba5
Michal Mlejnek <chaemil72@gmail.com>
2016-02-02 22:28:02 +0100

    initial commit

---
+++ app/src/main/java/cz/mapnik/app/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+package cz.mapnik.app.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chaemil on 2.2.16.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/cz/mapnik/app/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package cz.mapnik.app.activity;
+
+import android.os.Bundle;
+
+import cz.mapnik.app.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
