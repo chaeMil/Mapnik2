7a935625596d91bdaa58e94b1533ccaef755e580
Michal Mlejnek <chaemil72@gmail.com>
2016-02-02 22:51:02 +0100

    immersive view

--- app/src/main/java/cz/mapnik/app/activity/BaseActivity.java
+++ app/src/main/java/cz/mapnik/app/activity/BaseActivity.java
@@ -1,15 +1,53 @@
 package cz.mapnik.app.activity;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by chaemil on 2.2.16.
  */
 public class BaseActivity extends AppCompatActivity {
 
+    private View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getUI();
+
+        setupUI();
+    }
+
+    private void getUI() {
+        decorView = getWindow().getDecorView().getRootView();
+    }
+
+    private void setupUI() {
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+            } else {
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            }
+        }
     }
 }
