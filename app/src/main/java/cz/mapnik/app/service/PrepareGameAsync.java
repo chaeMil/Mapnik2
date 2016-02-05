c2e33c05d0e828d59bf7c159f17ef01405e4e111
Michal Mlejnek <chaemil72@gmail.com>
2016-02-05 22:52:06 +0100

    fix async preparing of game

--- app/src/main/java/cz/mapnik/app/service/PrepareGameAsync.java
+++ app/src/main/java/cz/mapnik/app/service/PrepareGameAsync.java
@@ -9,6 +9,7 @@ import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
 
+import cz.mapnik.app.R;
 import cz.mapnik.app.activity.BaseActivity;
 import cz.mapnik.app.model.Game;
 import cz.mapnik.app.model.Guess;
@@ -22,19 +23,24 @@ import cz.mapnik.app.utils.SmartLog;
  */
 public class PrepareGameAsync extends AsyncTask{
 
-    private static final int DEFAULT_GUESS_RADIUS = 800;
+    public static final String ERROR_LINK = "http://maps.googleapis.com/maps/api/streetview?size=400x400&location=34.834806,-41.314475";
+    public static final int DEFAULT_GUESS_RADIUS = 800;
+    public static final int ROUNDS = 5;
+    public static final int TRIES = 3;
+    public static final int MIN_VALID_GUESSES = 5;
+    public static final double MIN_LAT = -90;
+    public static final double MAX_LAT = 90;
+    public static final double MIN_LNG = -180;
+    public static final double MAX_LNG = 180;
+
+    private String errorString;
     private Game game;
     private BaseActivity caller;
     ArrayList<Guess> guesses = new ArrayList<>();
     private int cycles = 0;
     private boolean boundsAvailable = false;
-
-    private int ROUNDS = 5;
-    private int TRIES = 3;
-    private double MIN_LAT = -90;
-    private double MAX_LAT = 90;
-    private double MIN_LNG = -180;
-    private double MAX_LNG = 180;
+    private String apiKey;
+    private ArrayList<AsyncTask> childAsyncTasks;
 
     public PrepareGameAsync(Game game, BaseActivity caller) {
         this.game = game;
@@ -44,6 +50,16 @@ public class PrepareGameAsync extends AsyncTask{
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+
+        apiKey = caller.getResources().getString(R.string.api_key);
+        childAsyncTasks = new ArrayList<>();
+
+        Ion.with(caller).load(ERROR_LINK + "&key=" + apiKey).asString().setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+                errorString = result;
+            }
+        });
 
         caller.setMaxPreparationSteps(ROUNDS * TRIES);
     }
@@ -104,58 +120,52 @@ public class PrepareGameAsync extends AsyncTask{
                                     final double minLat, final double maxLat, final double minLng, final double maxLng,
                                     final int radius) {
 
-        final String errorLink = "http://maps.googleapis.com/maps/api/streetview?size=400x400&location=34.834806,-41.314475";
-        final String baseLink = "http://maps.googleapis.com/maps/api/streetview?size=400x400&sensor=true&location=";
+        final String baseLink = "http://maps.googleapis.com/maps/api/streetview?size=400x400&sensor=true&location=" + lat + "," + lng + "&key=" + apiKey;
 
-        Ion.with(caller).load(errorLink).asString().setCallback(new FutureCallback<String>() {
-
-
+        Ion.with(context).load(baseLink).asString().setCallback(new FutureCallback<String>() {
             @Override
-            public void onCompleted(Exception e, final String errorString) {
+            public void onCompleted(Exception e, String result) {
+                if (result != null && errorString != null) {
 
+                    SmartLog.Log(SmartLog.LogLevel.DEBUG, "link", baseLink);
 
-                Ion.with(context).load(baseLink + lat + "," + lng).asString().setCallback(new FutureCallback<String>() {
-                    @Override
-                    public void onCompleted(Exception e, String result) {
-                        if (result != null && errorString != null) {
+                    cycles += 1;
 
-                            SmartLog.Log(SmartLog.LogLevel.DEBUG, "link", baseLink + lat + "," + lng);
+                    caller.increaseCurrentPreparationStep();
 
-                            if (errorString.equals(result)) {
-                                SmartLog.Log(SmartLog.LogLevel.DEBUG, "available", "false");
+                    SmartLog.Log(SmartLog.LogLevel.DEBUG, "cycles", String.valueOf(cycles));
 
-                            } else {
-                                SmartLog.Log(SmartLog.LogLevel.DEBUG, "available", "true");
+                    if (errorString.equals(result)) {
+                        SmartLog.Log(SmartLog.LogLevel.DEBUG, "available", "false");
 
-                                CreateGuessAsync createGuessAsync;
-
-                                cycles += 1;
-
-                                caller.increaseCurrentPreparationStep();
-
-                                boolean lastOne = false;
-
-                                if (cycles == ROUNDS * TRIES) {
-                                    lastOne = true;
-                                }
-
-                                if (boundsAvailable) {
-                                    createGuessAsync = new CreateGuessAsync(PrepareGameAsync.this,
-                                            context, lat, lng, minLat, maxLat, minLng, maxLng, lastOne);
-                                } else {
-                                    createGuessAsync = new CreateGuessAsync(PrepareGameAsync.this,
-                                            context, lat, lng, radius, lastOne);
-                                }
-
-                                createGuessAsync.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-
-
-                            }
+                        if (cycles == ROUNDS * TRIES) {
+                            preparationFinished();
                         }
+
+                    } else {
+                        SmartLog.Log(SmartLog.LogLevel.DEBUG, "available", "true");
+
+                        CreateGuessAsync createGuessAsync;
+
+                        boolean lastOne = false;
+
+                        if (cycles == ROUNDS * TRIES) {
+                            lastOne = true;
+                        }
+
+                        if (boundsAvailable) {
+                            createGuessAsync = new CreateGuessAsync(PrepareGameAsync.this,
+                                    context, lat, lng, minLat, maxLat, minLng, maxLng, lastOne);
+                        } else {
+                            createGuessAsync = new CreateGuessAsync(PrepareGameAsync.this,
+                                    context, lat, lng, radius, lastOne);
+                        }
+
+                        AsyncTask childAsync = createGuessAsync.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                        childAsyncTasks.add(childAsync);
+
                     }
-                });
-
-
+                }
             }
         });
 
@@ -166,7 +176,14 @@ public class PrepareGameAsync extends AsyncTask{
     }
 
     public void preparationFinished() {
-        if (cycles == ROUNDS * TRIES) {
+
+        for (int a = 0; a < childAsyncTasks.size(); a++) {
+            if (childAsyncTasks.get(a) != null) {
+                childAsyncTasks.get(a).cancel(true);
+            }
+        }
+
+        if (guesses.size() >= MIN_VALID_GUESSES) {
             caller.gamePreparationFinished(guesses);
         }
     }
