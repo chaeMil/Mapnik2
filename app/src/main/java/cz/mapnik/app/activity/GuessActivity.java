4c16d59e8783a28beeb182ce471a62236f833ac5
Michal Mlejnek <chaemil72@gmail.com>
2016-02-05 01:33:07 +0100

    added basic guess activity init

--- app/src/main/java/cz/mapnik/app/activity/GuessActivity.java
+++ app/src/main/java/cz/mapnik/app/activity/GuessActivity.java
@@ -1,22 +1,48 @@
 package cz.mapnik.app.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 
 import java.util.ArrayList;
 
+import at.markushi.ui.CircleButton;
 import cz.mapnik.app.Mapnik;
 import cz.mapnik.app.R;
 import cz.mapnik.app.model.Game;
 import cz.mapnik.app.model.Guess;
+import cz.mapnik.app.model.Player;
+import cz.mapnik.app.utils.SmartLog;
 
 /**
  * Created by chaemil on 2.2.16.
  */
-public class GuessActivity extends BaseActivity {
+public class GuessActivity extends BaseActivity implements OnStreetViewPanoramaReadyCallback, View.OnClickListener {
 
+    private static final int MAX_TURNS = 5;
 
     private ArrayList<Guess> guesses;
     private Game game;
+    private StreetViewPanoramaFragment streetView;
+    private ArrayList<Player> players;
+
+    private int currentTurn = 0;
+    private int currentPlayer = 0;
+
+    private int maxTurns;
+    private RelativeLayout currentTurnWrapper;
+    private RelativeLayout nextPlayerWrapper;
+    private ImageView nextPlayerAvatar;
+    private TextView nextPlayerNick;
+    private CircleButton nextPlayerConfirm;
+    private StreetViewPanorama panorama;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +51,63 @@ public class GuessActivity extends BaseActivity {
 
         game = ((Mapnik) getApplication()).getCurrentGame();
         guesses = game.getGuesses();
+        players = ((Mapnik) getApplication()).getPlayers();
+        maxTurns = MAX_TURNS;
 
+        getUI();
+
+        setupUI();
+
+        nextPlayer();
+
+    }
+
+    private void getUI() {
+        streetView = (StreetViewPanoramaFragment) getFragmentManager()
+                .findFragmentById(R.id.streetView);
+        streetView.getStreetViewPanoramaAsync(this);
+        currentTurnWrapper = (RelativeLayout) findViewById(R.id.currentTurnWrapper);
+        nextPlayerWrapper = (RelativeLayout) findViewById(R.id.nextPlayerWrapper);
+        nextPlayerAvatar = (ImageView) findViewById(R.id.nextPlayerAvatar);
+        nextPlayerNick = (TextView) findViewById(R.id.nextPlayerNick);
+        nextPlayerConfirm = (CircleButton) findViewById(R.id.nextPlayerConfirm);
+    }
+
+    private void setupUI() {
+        nextPlayerConfirm.setOnClickListener(this);
+    }
+
+    private void nextPlayer() {
+
+        Player player = players.get(currentTurn);
+
+        currentTurnWrapper.setVisibility(View.GONE);
+        nextPlayerWrapper.setVisibility(View.VISIBLE);
+        int avatarRes = getResources().getIdentifier(player.getAvatar(), "drawable", getPackageName());
+        nextPlayerAvatar.setImageDrawable(getResources().getDrawable(avatarRes));
+        nextPlayerNick.setText(player.getName());
+
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        SmartLog.Log(SmartLog.LogLevel.DEBUG, "location", guesses.get(0).getLocation().toString());
+
+        this.panorama = panorama;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.nextPlayerConfirm:
+                if (currentPlayer < players.size()) {
+
+                    nextPlayerWrapper.setVisibility(View.GONE);
+                    currentTurnWrapper.setVisibility(View.VISIBLE);
+                    panorama.setPosition(guesses.get(currentTurn).getLocation(), 200);
+
+                }
+                break;
+        }
     }
 }
